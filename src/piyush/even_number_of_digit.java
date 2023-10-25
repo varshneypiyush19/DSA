6d1058c365757d6c5f9a2bb188c63f3ba8b470b4
varshneypiyush19 <varshneypiyush2004@gmail.com>
2023-10-25 23:09:29 +0530

    24-10-23

---
+++ src/piyush/even_number_of_digit.java
@@ -0,0 +1,38 @@
+// find the digits which contain even number of digits
+
+package piyush;
+
+public class even_number_of_digit {
+    public static void main(String[] args) {
+       int[] nums ={12,345,27,6,7896};
+        System.out.println( evendigits(nums));
+    }
+
+    static int evendigits(int[] nums){
+        int count=0;
+        for(int num :nums){
+            if(even(num)){
+                count++;
+            }
+         }
+        return count;
+          }
+
+ static boolean even(int num) {
+int numberofdigits =digits(num);
+     return numberofdigits % 2 == 0;
+ }
+
+ static int digits(int num){
+
+        int count=0;
+        while(num>0){
+           count++;
+           num=num/10;
+        }
+        return count;
+ }
+
+
+}
+
