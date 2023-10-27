737f9134cab6363a5c30796f670ce305a2c3758d
varshneypiyush19 <varshneypiyush2004@gmail.com>
2023-10-28 00:20:40 +0530

    27-10-23

---
+++ src/piyush/binarysearch.java
@@ -0,0 +1,33 @@
+package piyush;
+
+public class binarysearch {
+    public binarysearch() {
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, 6, 8, 9, 23, 45, 67, 99};
+        int target = 23;
+        int ans = binary_search(arr, target);
+        System.out.println(ans);
+    }
+
+    static int binary_search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                if (target >= arr[mid]) {
+                    return mid;
+                }
+
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
