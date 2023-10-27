8d1842f8fe7ce99da89f2fc305226d72ebf44c96
varshneypiyush19 <varshneypiyush2004@gmail.com>
2023-10-28 00:35:58 +0530

    27-10-23

--- src/piyush/binarysearch.java
+++ src/piyush/binarysearch.java
@@ -19,12 +19,13 @@ public class binarysearch {
             int mid = start + (end - start) / 2;
             if (target > arr[mid]) {
                 start = mid + 1;
-            } else {
-                if (target >= arr[mid]) {
-                    return mid;
-                }
-
+            }
+            else if (target < arr[mid]) {
                 end = mid - 1;
+                }
+            else{
+                return mid;
+
             }
         }
 
