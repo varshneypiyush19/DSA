1f325e8f42e2cc9b7ab0f691481e2faeacec052b
varshneypiyush19 <varshneypiyush2004@gmail.com>
2023-11-01 00:46:54 +0530

    31-10-23

--- src/piyush/SIMILAR_TO_CEILING.java
+++ src/piyush/SIMILAR_TO_CEILING.java
@@ -9,26 +9,25 @@ package piyush;
 
 public class SIMILAR_TO_CEILING {
     public static void main(String[] args) {
-       int[] arr={'a','b','c','g', 'l','z'};
-       int  target = 'a';
-      int ans =  search(arr,target);
+       int[] arr={'a','b','c','g', 'l','x'};
+       int  target = 'z';
+      char ans = (char) search(arr,target);
         System.out.println(ans);
     }
 
     static int search(int[] arr,int target){
         int start =0;
         int end = arr.length - 1;
-        int mid= start +(end-start)/2;
 
         while(start<=end){
-                    if(target>mid){
+            int mid= start +(end-start)/2;
+                    if(target>arr[mid]){
                 start = mid+1;
             }
-            else if(target<mid){
+            else if (target<arr[mid]) {
                 end=mid-1;
             }
         }
-
-        return mid;
+        return arr[start%arr.length];
     }
 }
