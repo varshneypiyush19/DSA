ab3c35d02e92f1a540cd33e6ae9d2f694608a147
varshneypiyush19 <varshneypiyush2004@gmail.com>
2023-11-04 00:29:09 +0530

    3-11-23

--- src/piyush/peak_element.java
+++ src/piyush/peak_element.java
@@ -22,14 +22,13 @@ public class peak_element {
         int start=0;
         int end=arr.length-1;
         while(start<end){
-         int mid = start + (end-start)/2;
+            int mid = start + (end-start)/2;
             if(arr[mid]>arr[mid+1]){
                 end=mid;
                 }
-            else if (arr[mid]<arr[mid+1]){
+            else if(arr[mid]<arr[mid+1]){
                 start=mid+1;
                 }
-
         }
         return arr[start];
     }
